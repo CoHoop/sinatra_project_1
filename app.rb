a267e69efccb3392b1266237f605fb950f6c4748
Mathilde <mathildeleo@gmail.com>
2013-07-30 20:10:14 +0200

    test

--- app.rb
+++ app.rb
@@ -1,16 +1,14 @@
 require 'sinatra'
 
-
 get '/' do
     erb :city
 end
 
 
 get '/:city' do
-    city = params[:city].capitalize
+    city = params[:city].split('').map(&:capitalize).join(' ')
     @message = "#{city}"
     erb :index
 end
-
 
 
